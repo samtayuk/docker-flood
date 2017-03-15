11e030ea95026688b5abb6be390d85cfa60cd676
Samuel Taylor <samtaylor.uk@gmail.com>
2017-03-15 21:34:42 +0000

    update

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   dbPath: './server/db/',
   floodServerPort: 3000,
   maxHistoryStates: 30,
-  pollInterval: 1000 * 5,
+  pollInterval: 1000,
   secret: 'flood',
   scgi: {
     host: 'rtorrent',
